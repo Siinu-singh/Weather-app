d378388fee623ee3d3954b1e83ec3e70c45d86ff
=suraj <=>
2025-03-29 18:36:48 +0530

    Fixed import issue for searchBox

--- src/weatherInfo.jsx
+++ src/weatherInfo.jsx
@@ -5,26 +5,28 @@ import InfoBox from "./infoBox";
 import { useState } from "react";
 
 export default function WeatherInfo() {
-    const[weatherInfo, setWeatherInfo] = useState({
-        city: "Delhi",
-        temp: 25,
-        humidity: 60,
-        temp_min: 20,
-        temp_max: 30,
-        pressure: 1010,
-        description: "Clear sky",
-        sunset: 1670000000,
-    });
+  const [weatherInfo, setWeatherInfo] = useState({
+    city: "Delhi",
+    temp: 25,
+    humidity: 60,
+    temp_min: 20,
+    temp_max: 30,
+    pressure: 1010,
+    description: "Clear sky",
+    sunset: 1670000000,
+  });
 
-    let updateWeatherInfo = async (result) => {
-        setWeatherInfo(result); 
-    }
+  let updateWeatherInfo = async (result) => {
+    setWeatherInfo(result);
+  };
   return (
     <>
-    <h2>Weather App By <i>Suraj Kumar</i></h2>
-      <SearchBox updateWeatherInfo={updateWeatherInfo}/>
+      <h2>
+        Weather App By <i>Suraj Kumar</i>
+      </h2>
+      <SearchBox updateWeatherInfo={updateWeatherInfo} />
       <br />
-      <InfoBox info={weatherInfo}/>
+      <InfoBox info={weatherInfo} />
     </>
   );
 }
